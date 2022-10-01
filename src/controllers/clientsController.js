bb23fa48cc6b69e1e0daf78b51da12776162b57c
ANDERSON BRUNO LOPES VALE <andersonbruno370@gmail.com>
2022-10-01 09:40:33 -0300

    Adicionado funcinalidades de editar(com sua pagina e estilo) e remover cadastro rotas para cada função

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -6,18 +6,67 @@ function add(req, res) {
 
     const { name, email, skills } = req.body;
 
-    const register = new ClientsModel({
-        name,
-        email,
-        skills,
+    if (name !== "" || name !== null) {
+
+        const register = new ClientsModel({
+            name,
+            email,
+            skills,
+        });
+
+        register.save();
+
+        res.redirect('/');
+    }
+
+
+}
+
+async function editIndex(req, res) {
+    const { id } = req.query;
+
+    const cliente = await ClientsModel.findById(id);
+
+    res.render('editClient', {
+        cliente,
+    });
+}
+
+async function edit(req, res) {
+
+    const { name, email, skills } = req.body;
+
+
+    const { id } = req.params;
+
+    const cliente = await ClientsModel.findById(id);
+
+    cliente.name = name;
+    cliente.email = email;
+    cliente.skills = skills;
+
+    cliente.save();
+
+    res.render('editClient', {
+        cliente,
     });
 
-    register.save();
-    
-    res.redirect('/');
+}
 
+async function remove(req, res) {
+
+    const { id } = req.params;
+
+    const remover = await ClientsModel.deleteOne({ _id: id });
+
+    if (remover.deletedCount === 1) {
+        res.redirect('/')
+    }
 }
 
 module.exports = {
     add,
+    editIndex,
+    edit,
+    remove,
 }
--- src/routers/index.js
+++ src/routers/index.js
@@ -5,6 +5,13 @@ const ClientsController = require('../controllers/clientsController')
 
 router.get('/', IndexController.index);
 
-router.post('/add', ClientsController.add)
+router.post('/add', ClientsController.add);
+
+//Editar
+router.get('/edit', ClientsController.editIndex);
+router.post('/edit/:id', ClientsController.edit);
+
+//remover
+router.get('/remove/:id', ClientsController.remove);
 
 module.exports = router; 
